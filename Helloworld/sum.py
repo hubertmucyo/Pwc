cfc2ed07cd251374e5f35fb3ebfcf754a1a76451
Hubert Mucyo <h.mucyo@alustudent.com>
2023-10-16 13:18:03 +0200

    sum

---
+++ Helloworld/sum.py
@@ -0,0 +1,7 @@
+#this code uses sum
+print("enter three numbers:\n")
+num1=float(input("number 1:"))
+num2=float(input("number 2:"))
+num3=float(input("number 3:"))
+addition=(num1+num2+num3)
+print("the sum is ",addition)
