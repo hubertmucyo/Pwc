e1573c9d11f090dfdb87d75fe1474956886e6a96
Hubert Mucyo <h.mucyo@alustudent.com>
2023-10-16 13:47:44 +0200

    sub

---
+++ Helloworld/sub.py
@@ -0,0 +1,11 @@
+#this program uses subtraction
+def sub(num1,num2):
+result=num2-num1
+return result
+print("Enter two numbers: ")
+a=float(input("num1: "))
+b=float(input("num2: "))
+div=a-b
+print("subtracting the first number to the second:",div)
+subtraction=sub(a,b)
+print("subtracting the second number to the first:",subtraction)
