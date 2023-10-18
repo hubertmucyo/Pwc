5f88efcee45a39e7f370135bec80a9523904a0bf
Hubert Mucyo <h.mucyo@alustudent.com>
2023-10-18 10:34:40 +0200

    turtle program begin

---
+++ fundamentals/turtle1.py
@@ -0,0 +1,11 @@
+#This program uses turtle
+import turtle
+wn=turtle.Screen()
+hubert=turtle.Turtle()
+dist=50
+angle=90
+for a in range(15):
+    hubert.forward(dist)
+    hubert.right(angle)
+    dist+=10
+    angle-=3
