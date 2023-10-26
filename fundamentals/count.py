1b09314d76b4f9fc357a12a085a225a2c722e38c
Hubert Mucyo <h.mucyo@alustudent.com>
2023-10-26 13:06:33 +0200

    count method

---
+++ fundamentals/count.py
@@ -0,0 +1,13 @@
+#this program explores count method
+print("welcome!!! What is your name? ")
+name=input("Enter your names: ")
+print("The letter e appears in your name the followig times: "
+,name.count("e"))
+print("The letter a appears in your name the followig times: "
+,name.count("a"))
+print("The letter i appears in your name the followig times: "
+,name.count("i"))
+print("The letter u appears in your name the followig times: "
+,name.count("u"))
+print("The letter o appears in your name the followig times: "
+,name.count("o"))
--- fundamentals/dice.py
+++ fundamentals/dice.py
@@ -3,3 +3,7 @@ print("welcome! This program helps you to roll a dice logically!")
 import random
 dice=random.randrange(1,7)
 print(dice)
+#or
+# from random import randrange
+# dice=randrange(1,7)
+# print(dice)
