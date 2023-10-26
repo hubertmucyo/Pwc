87665442f0d660961d9743f2521dac634bcbfea8
Hubert Mucyo <h.mucyo@alustudent.com>
2023-10-26 13:38:35 +0200

    index and split  method

---
+++ fundamentals/index.py
@@ -0,0 +1,13 @@
+#This program uses index method
+name="Adolph Blaine Charles David Earl Frederick Gerald Hubert Irvin John Kenneth Lloyd Martin Nero Oliver Paul Quincy Randolph Sherman Thomas Uncas Victor William Xerxes Yancy Zeus Wolfeschlegel­steinhausen­bergerdorff­welche­vor­altern­waren­gewissenhaft­schafers­wessen­schafe­waren­wohl­gepflege­und­sorgfaltigkeit­beschutzen­vor­angreifen­durch­ihr­raubgierig­feinde­welche­vor­altern­zwolfhundert­tausend­jahres­voran­die­erscheinen­von­der­erste­erdemensch­der­raumschiff­genacht­mit­tungstein­und­sieben­iridium­elektrisch­motors­gebrauch­licht­als­sein­ursprung­von­kraft­gestart­sein­lange­fahrt­hinzwischen­sternartig­raum­auf­der­suchen­nachbarschaft­der­stern­welche­gehabt­bewohnbar­planeten­kreise­drehen­sich­und­wohin­der­neue­rasse­von­verstandig­menschlichkeit­konnte­fortpflanzen­und­sich­erfreuen­an­lebenslanglich­freude­und­ruhe­mit­nicht­ein­furcht­vor­angreifen­vor­anderer­intelligent­geschopfs­von­hinzwischen­sternartig­raum "
+print("following is the world record for the longest name in the world: ")
+print(name)
+#split method
+print("let us split the name ")
+names=name.split()
+print(names)
+#index method 
+#It works for both string and list
+alphabet=input("enter any letter or char you want to see its number of apparition: " )
+print("The letter or character appears the following times: ")
+print(name.index(alphabet))
